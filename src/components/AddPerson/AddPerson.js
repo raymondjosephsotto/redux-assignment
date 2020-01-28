a7be97679bb8a7970007edf167f95e6fce623138
Raymond Sotto <raymondjosephsotto@gmail.com>
2020-01-28 14:22:52 -0500

    Added Input and output features to add Person

--- src/components/AddPerson/AddPerson.js
+++ src/components/AddPerson/AddPerson.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import './AddPerson.css';
 
-const addPerson = (props) => (
-    <div className="AddPerson">
-        <button className="button" onClick={props.personAdded}>Add Person</button>
-    </div>
-);
 
-export default addPerson;
+class AddPerson extends Component {
+    state = {
+        name: '',
+        age: ''
+    }
+
+    nameChangeHandler = e => {
+        this.setState({name: e.target.value})
+    }
+
+    ageChangeHandler = e => {
+        this.setState({age: e.target.value})
+    }
+
+    render(){
+    return(
+    <div className="AddPerson">
+        <input type="text" placeholder="Name" onChange={this.nameChangeHandler} value={this.state.name}/>
+        <input type="number" placeholder="Age" onChange={this.ageChangeHandler} value={this.state.age}/>
+        <button className="button" onClick={() => this.props.personAdded(this.state.name, this.state.age)}>Add Person</button>
+    </div>
+        );
+    }
+}
+
+export default AddPerson;
